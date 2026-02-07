5ef1c873cc6abbe3ff821cb32c7daec695d7df38
Jonta254 <141897131+Jonta254@users.noreply.github.com>
2026-02-07 13:15:54 +0300

    Update app.js Display updates with external links
    Display updates with external links

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const feed = document.getElementById("feed");
+fetch("updates.json")
+  .then(res => res.json())
+  .then(data => {
+    const feed = document.getElementById("feed");
+    feed.innerHTML = "";
 
-function post() {
-  const text = document.getElementById("text").value;
-  if (!text) return;
-
-  const div = document.createElement("div");
-  div.className = "post";
-  div.innerHTML = `<p>${text}</p>
-  <input placeholder="Comment..." onkeypress="addComment(event,this)">
-  <div class="comments"></div>`;
-
-  feed.prepend(div);
-  document.getElementById("text").value = "";
-}
-
-function addComment(e,input){
-  if(e.key==="Enter"){
-    const c = document.createElement("p");
-    c.innerText = input.value;
-    input.nextElementSibling.appendChild(c);
-    input.value="";
-  }
-}
+    data.forEach(update => {
+      const div = document.createElement("div");
+      div.className = "post";
+      div.innerHTML = `
+        <h4>${update.title}</h4>
+        <p>${update.content}</p>
+        <a href="${update.link}" target="_blank">Read more</a>
+        <br />
+        <small>${update.time}</small>
+      `;
+      feed.appendChild(div);
+    });
+  });
